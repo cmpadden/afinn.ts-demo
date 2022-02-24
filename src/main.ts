a1ae1a6be3edd026d6daf38cd548088b0289b3b8
Colton Padden <colton.padden@fastmail.com>
2022-02-24 10:43:00 -0500

    dynamically updated annotated text

--- src/main.ts
+++ src/main.ts
@@ -9,8 +9,14 @@ const app = document.querySelector<HTMLDivElement>("#app")!;
 
 const sentences = [
   { lang: "en", sentence: "This is utterly excellent!" },
-  { lang: "da", sentence: "Hvis ikke det er det mest afskyelige flueknepperi...", },
-  { lang: "fi", sentence: "Siellä on uusi hyvä juttu, katsokaa ja kuunnelkaa ihmeessä.", },
+  {
+    lang: "da",
+    sentence: "Hvis ikke det er det mest afskyelige flueknepperi...",
+  },
+  {
+    lang: "fi",
+    sentence: "Siellä on uusi hyvä juttu, katsokaa ja kuunnelkaa ihmeessä.",
+  },
   { lang: "sv", sentence: "det är inte bra" },
   { lang: "tr", sentence: "iyi değil" },
   { lang: "emoticons", sentence: "I saw that yesterday :)" },
@@ -41,15 +47,46 @@ app.append(table);
 //                                                   Free-form                                                    //
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+/**
+ * Surround `word` occurrences in `text` with span tags and specified `class`
+ */
+function highlightWords(
+  text: string,
+  word: string,
+  classes?: string,
+  title?: string
+) {
+  const pattern = "(?<=^|[\\P{L}])(" + word + ")(?=$|[\\P{L}])";
+  return text.replace(
+    new RegExp(pattern, "gmui"),
+    `<span class="${classes}" title="${title}">$1</span>`
+  );
+}
+
+/**
+ * Compute scores, and then highlight words given their score by wrapping them in <span> elements
+ */
+function constructAnnotatedText(text: string): string {
+  const scores = afinn.scores(input.value);
+  scores.forEach((s) => {
+    const classes = s.score > 0 ? "positive" : s.score < 0 ? "negative" : "";
+    text = highlightWords(text, s.word, classes, s.score.toString());
+  });
+  return text;
+}
+
 const afinn = new Afinn({ language: "en" });
 
-const input = document.querySelector("#text-input") as HTMLTextAreaElement;
-const score = document.querySelector("#score") as HTMLDivElement;
+const input = document.getElementById("text-input") as HTMLTextAreaElement;
+const annotatedText = document.getElementById("annotated-text") as HTMLDivElement;
+const score = document.getElementById("overall-score") as HTMLDivElement;
 
-// Initially set score
+// Initially set scores
 score.innerHTML = afinn.score(input.value).toString();
+annotatedText.innerHTML = constructAnnotatedText(input.value);
 
 // Setup listener to dynamically set score based on input text
 input.addEventListener("input", (_event) => {
   score.innerHTML = afinn.score(input.value).toString();
+  annotatedText.innerHTML = constructAnnotatedText(input.value);
 });
